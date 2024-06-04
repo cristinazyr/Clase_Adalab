d5cd2baaedb3f70d9e118dd5b0958fd68c3cafc5
cristinazyr <criscristina725@hotmail.com>
2024-06-04 18:25:27 +0200

    new

---
+++ Clase_Js-1/main.js
@@ -0,0 +1,28 @@
+"use strict";
+//Variables constantes
+
+//debugger es una herramienta para que revisemos desde aquí por linea
+//debugger
+
+
+console text = "Hola";
+
+console.log(text);
+
+//Variables let
+let number = 1;
+
+console.log(number);
+
+let a=10
+let b =20
+let c =a+b
+
+//cambiar una imagen de html 
+const imagen = document. querySelector('.ima__js')
+console.log(imagen)
+imagen.src=""
+
+//innerHTML
+const paragrah =document.querySelector(".js__paragrah")
+paragrah.innerHTML= "<h2>Holis</h2> "
